2fc26adccd4cd7560213551e9e62e3c7ea56d601
Iván <Ivn@DESKTOP-62SG547>
2025-05-24 20:55:23 +0200

    Añade aplicación completa (falta refactorizar y añadir algunos retoques)

--- BiblioApp/src/main/java/controlador/PerfilController.java
+++ BiblioApp/src/main/java/controlador/PerfilController.java
@@ -1,18 +1,75 @@
 package controlador;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.hibernate.Session;
 
 import app.BiblioApp;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import modelo.Alquiler;
+import modelo.Libro;
+import modelo.ManagerPrincipal;
+import modelo.Usuario;
 
 public class PerfilController {
 	
 	@FXML
-	private void regresar() throws IOException {
-		if (LoginController.esAdmin == true) {
-			BiblioApp.setRoot("menuAdmin");
-		} else {
-			BiblioApp.setRoot("menu");
+	private TableView<Alquiler> tablaAlquileres;
+
+	@FXML
+	private TableColumn<Alquiler, LocalDate> colFechaAlq;
+	
+	@FXML
+	private TableColumn<Alquiler, LocalDate> colFechaPrev;
+
+	@FXML
+	private TableColumn<Alquiler, LocalDate> colFechaDev;
+
+	@FXML
+	private TableColumn<Alquiler, String> colLibros;
+
+	private Usuario usuario;
+	
+	@FXML
+	private void initialize() {
+
+		colFechaAlq.setCellValueFactory(new PropertyValueFactory<>("fechaAlquiler"));
+		colFechaPrev.setCellValueFactory(new PropertyValueFactory<>("fechaPrevista"));
+		colFechaDev.setCellValueFactory(new PropertyValueFactory<>("fechaDevolucion"));
+
+		colLibros.setCellValueFactory(cd -> new SimpleStringProperty(
+				cd.getValue().getLibros().stream().map(Libro::getNombre_libro).collect(Collectors.joining(", "))));
+		
+		cargarAlquileres();
+
+	}
+	
+	private void cargarAlquileres() {
+		usuario = LoginController.getUsuarioActual();
+		
+		try (Session session = ManagerPrincipal.getSessionFactory().openSession()) {
+
+			List<Alquiler> alquileres = session
+					.createQuery("SELECT DISTINCT a " + "FROM Alquiler a " + "LEFT JOIN FETCH a.libros "
+							+ "WHERE a.usuario.id = :uid", Alquiler.class)
+					.setParameter("uid", usuario.getId_usuario()).list();
+
+			tablaAlquileres.setItems(FXCollections.observableArrayList(alquileres));
 		}
+	}
+	
+	@FXML
+	private void regresar() throws IOException {
+		BiblioApp.setRoot("menu");
 	}
 }
